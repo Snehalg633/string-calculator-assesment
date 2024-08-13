2631979f22a5c68945816118f69b85aca5343f3b
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2024-08-13 08:02:48 +0530

    Implement logic to support newlines as delimiters

--- src/services/stringCalculator.ts
+++ src/services/stringCalculator.ts
@@ -1,5 +1,5 @@
 export function add(numbers: string): number {
   if (!numbers) return 0;
-  const numbArray = numbers.split(",").map(Number);
+  const numbArray = numbers.split(/[\n,]/).map(Number);
   return numbArray.reduce((acc, num) => acc + num, 0);
 }
