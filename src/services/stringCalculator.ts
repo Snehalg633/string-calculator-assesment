39b223dbac8c01887782e3892ad28106747fc0cd
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2024-08-13 08:13:24 +0530

    Logic to support custom delimiters

--- src/services/stringCalculator.ts
+++ src/services/stringCalculator.ts
@@ -1,5 +1,12 @@
 export function add(numbers: string): number {
   if (!numbers) return 0;
-  const numbArray = numbers.split(/[\n,]/).map(Number);
-  return numbArray.reduce((acc, num) => acc + num, 0);
+
+  let delimiter = /[\n,]/;
+  if (numbers.startsWith("//")) {
+    const newNum = numbers.split("\n", 2);
+    delimiter = new RegExp(newNum[0].slice(2));
+    numbers = newNum[1];
+  }
+  const numArray = numbers.split(delimiter).map(Number);
+  return numArray.reduce((acc, num) => acc + num, 0);
 }
