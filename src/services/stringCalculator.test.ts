efac9acf7484e5b23b2852d04a70c339df1c0fc9
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2024-08-13 08:16:06 +0530

    Failing test case for negative numbers

--- src/services/stringCalculator.test.ts
+++ src/services/stringCalculator.test.ts
@@ -24,3 +24,15 @@ test('returns sum for numbers with custom delimiter', () => {
     expect(add('//;\n1;2')).toBe(3);
 });
 
+test('returns sum for numbers with custom delimiter', () => {
+    expect(add('//;\n1;2')).toBe(3);
+});
+
+test('throws error for negative numbers', () => {
+    expect(() => add('1,-2,3')).toThrow('Negative numbers not allowed: -2');
+});
+
+test('throws error for multiple negative numbers', () => {
+    expect(() => add('1,-2,-3,4')).toThrow('Negative numbers not allowed: -2, -3');
+});
+
