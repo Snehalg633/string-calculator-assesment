1a7f8e5301edb2e84ff145aee195761cfa6f3327
Snehalg633 <92521212+Snehalg633@users.noreply.github.com>
2024-08-13 07:28:14 +0530

    Implement basic string addition logic in stringCalculator service

---
+++ src/components/StringCalculator.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {add} from "../services/stringCalculatro"
+
+function StringCalculator(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default StringCalculator;
---
+++ src/services/stringCalculatro.ts
@@ -0,0 +1,5 @@
+export function add(numbers: string): number {
+  if (numbers === "") return 0;
+  const numbArray = numbers.split(",").map(Number);
+  return numbArray.reduce((acc, num) => acc + num, 0);
+}
